d1efe59f9404ac4cb3d057fa28db01f62e9961d6
Shivam Bansal <shivam_b@ee.iitr.ac.in.com>
2025-03-14 17:39:25 +0530

    Requirement added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup 
+  
+setup( 
+    name='Information_Retriever_System', 
+    version='0.0.0', 
+    description='A sample Python package', 
+    author='Shivam bansal', 
+    author_email='shivam_b@ee.iitr.ac.in', 
+    packages=find_packages(), 
+    install_requires=[]
+) 
